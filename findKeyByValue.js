49c20ccf90afac3629468f65db8997f8ae025356
Raheel Bennett <raheel.bennett@telus.com>
2022-11-16 23:54:03 -0700

    completed findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) { // if actual and expected are identical in value and type then assertion passed.
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else { // if not identical then assertion failed.
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+const findKeyByValue = function (obj, val) {
+  for (key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
