9b4c36be83ca2467a01157362b5d387dc2394d9a
Raheel Bennett <raheel.bennett@telus.com>
2022-11-16 20:47:47 -0700

    completed letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+// We need a functon to compre each element of both arrays corresponding to their index and if either element don't match then false will be returned.
+const eqArrays = function(arr1, arr2) {
+  let x = true;
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      x = false;
+      return x;
+    }
+  } return x;
+};
+
+//assertArrayEqual will use eqArrays function to compare the two arrays to generate the appropriate assertion.
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) { // if actual and expected are identical in value and type then assertion passed.
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else { // if not identical then assertion failed.
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+const letterPositions = function(sentence) {
+  const results = {};
+  // We will loop through each letter, disregarding any spaces, if it does not already exists as a key in the final object then we will add it along with array of it's indices.
+  for (let index in sentence) {
+    if (sentence[index] !== " ") {
+      if (results[sentence[index]]) {
+        results[sentence[index]].push(Number(index));
+      } else {
+        results[sentence[index]] = [Number(index)];
+      }
+    }
+   
+  } return results;
+};
+
+const results = letterPositions("hello");
+assertArraysEqual(results["h"], [0]);
+assertArraysEqual(results["e"], [1]);
+assertArraysEqual(results["l"], [2,3]);
+assertArraysEqual(results["o"], [4]);
+
+
