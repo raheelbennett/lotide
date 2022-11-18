cb6dc0365ebcfd95033f1e27ea8c4bccf124991b
Raheel Bennett <raheel.bennett@telus.com>
2022-11-18 00:48:07 -0700

    completed Implement findKey

---
+++ findkey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) { // if actual and expected are identical in value and type then assertion passed.
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else { // if not identical then assertion failed.
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(test1, "noma");
+assertEqual(test1, "nomasssss"); // should fail
