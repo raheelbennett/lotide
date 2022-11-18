e90301008bb6d70f0f6c4f914c6a28a4041f90b3
Raheel Bennett <raheel.bennett@telus.com>
2022-11-17 22:20:39 -0700

    completed takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+
+// We need a functon to compre each element of both arrays corresponding to their index and if either element don't match then false will be returned.
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  } return true;
+};
+
+//assertArrayEqual will use eqArrays function to compare the two arrays to generate the appropriate assertion.
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) { // if actual and expected are identical in value and type then assertion passed.
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else { // if not identical then assertion failed.
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const returnArr = [];
+  for (let element of array) {
+    if (callback(element) === false) {
+      returnArr.push(element);
+    } else {
+      return returnArr;
+    }
+   
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood']);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood', "x"]); // ===> should fail
