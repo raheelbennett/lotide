423bea1db396ae34ff7ab385d540428c00750cdd
Raheel Bennett <raheel.bennett@telus.com>
2022-11-21 16:36:19 -0700

    Refactoring eqArrays done

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) { // if actual and expected are identical in value and type then assertion passed.
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else { // if not identical then assertion failed.
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // We need a functon to compre each element of both arrays corresponding to their index and if either element don't match then false will be returned.
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -18,7 +10,4 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);  // => should Fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", 4]), true);  // => should Fail
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);  // => should Fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", 4]), true);  // => should Fail
